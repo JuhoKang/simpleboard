08f3708752dcf7fde3119b6dfb031836e0222947
JuhoKang <rjduakcjs@gmail.com>
2016-07-13 02:33:42 +0900

    Modified AppController

--- src/main/java/kr/re/ec/exerciseboard/controller/AppController.java
+++ src/main/java/kr/re/ec/exerciseboard/controller/AppController.java
@@ -1,13 +1,19 @@
 package kr.re.ec.exerciseboard.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import kr.re.ec.exerciseboard.model.Post;
 import kr.re.ec.exerciseboard.model.User;
 import kr.re.ec.exerciseboard.model.UserProfileType;
+import kr.re.ec.exerciseboard.service.PostService;
 import kr.re.ec.exerciseboard.service.UserService;
 
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -20,6 +26,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -30,6 +37,9 @@ public class AppController {
   UserService userService;
 
   @Autowired
+  PostService postService;
+
+  @Autowired
   MessageSource messageSource;
 
   // root mapping
@@ -38,29 +48,50 @@ public class AppController {
     Authentication auth = SecurityContextHolder.getContext().getAuthentication();
     if (!(auth instanceof AnonymousAuthenticationToken)) {
       return "redirect:" + "/index";
-    } else {
-
     }
 
     return "redirect:" + "/login";
   }
-
-  @RequestMapping(value = {"/test"}, method = RequestMethod.GET)
-  public String test(ModelMap model) {
-    User user = userService.findBySso(getUserName());
-    model.addAttribute("user", user);
-
-    userService.deleteUserById(user.getId());
-
-    return "accessDenied";
+  @RequestMapping(value = {"/nosuchpage"}, method = RequestMethod.GET)
+  public String noSuchPage(ModelMap model) {
+    return "nosuchpage";
   }
 
   @RequestMapping(value = {"/index"}, method = RequestMethod.GET)
-  public String mainPage(ModelMap model) {
-    User user = userService.findBySso(getUserName());
-    model.addAttribute("user", user);
+  public String mainPage(ModelMap model,
+      @RequestParam(value = "page") Optional<Integer> page) {
+    List<Post> posts;
+    int maxPage = postService.getTotalPageLength();
+    
+    if(!page.isPresent()){
+      posts = postService.getPostsForPageN(1);
+    }else{
+      if(page.get() > maxPage){
+        return "redirect:/nosuchpage";
+      }
+      posts = postService.getPostsForPageN(page.get());
+    }
+    
+    model.addAttribute("maxpage",maxPage);
+    
+    model.addAttribute("posts", posts);
+    
     return "index";
   }
+  @RequestMapping(value = {"/add"}, method = RequestMethod.GET)
+  public String mainPage(ModelMap model) {
+    Post post = new Post();
+    post.setContent("아무것도");
+    post.setCreateTime(new DateTime());
+    post.setTitle("제목제목");
+    post.setWriterSsoId(getUserName());
+    postService.savePost(post);
+    
+    return "index";
+  }
+  
+  
+  
 
   @RequestMapping(value = {"/signup"}, method = RequestMethod.GET)
   public String newUser(ModelMap model) {
@@ -70,7 +101,7 @@ public class AppController {
     if (!(auth instanceof AnonymousAuthenticationToken)) {
       return "redirect:" + "/index";
     }
-    
+
     User user = new User();
     model.addAttribute("user", user);
     model.addAttribute("edit", false);
@@ -121,7 +152,7 @@ public class AppController {
     if (!(auth instanceof AnonymousAuthenticationToken)) {
       return "redirect:" + "/index";
     }
-    
+
     return "login";
   }
 
